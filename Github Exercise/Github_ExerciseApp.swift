9325aff197dd05efde23dadbb54b39172a603327
mhit30 <mohammedhit30@gmail.com>
2024-08-07 20:27:23 -0700

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by Mohammed Hit on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
